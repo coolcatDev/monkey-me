77224e3a05154fdc3be2b5ef8e43995103cac2b5
coolcatDev <alex_tsm@hotmail.com>
2015-02-05 21:10:08 +0200

    second commit

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+class BaseConfig(object): 
+	SECRET_KEY = "kjdsbfkjgdf78sft"
+	UPLOAD_FOLDER = 'static/uploads'
+
+	# LOCAL
+	SQLALCHEMY_DATABASE_URI= 'sqlite:///monkeyDB.db'
+
+	# Heroku
+	#SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
