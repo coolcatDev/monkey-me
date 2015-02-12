75776be1a26866cac9a1cf091be25acc084c9e1b
coolcatDev <alex_tsm@hotmail.com>
2015-02-12 22:35:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,4 +8,12 @@ class BaseConfig(object):
 	#SQLALCHEMY_DATABASE_URI= 'sqlite:///monkeyDB.db'
 
 	# Heroku
+	# Database
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+	# S3 BUCKET
+	S3_BUCKET = os.environ['S3_BUCKET']
+	# S3 ACCESS_KEY
+	AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
+	# S3 SECRET_KEY
+	AWS_SECRET_KEY = os.environ['AWS_SECRET_KEY']
+	
